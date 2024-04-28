b18178b8d5538abdbcd27fb4f47f50bbdcafef86
Victor <victorac2324@gmail.com>
2024-04-28 16:01:12 -0400

    did 5 deliverables

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
 class Pet:
-    pass
 
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all =[]
+
+    def __init__(self, name, pet_type, owner=None ):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        if pet_type not in self.PET_TYPES:
+            raise Exception("INVALID PET TYPE")
+       
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name =  name
+        self.all = []
+
+    def pets(self):
+        return self.all
+
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner =self
+            self.all.append(pet)
+        else:
+            raise Exception("Invalid pet Object")
+
+   
+
+    # def add_pet(self, pet):
+    #     if isinstance(pet, Pet):
+    #         pet.owner = self
+    #         self.allpets.append(pet)
+    #     else:
+    #         raise Exception("Invalid pet object")
+    
